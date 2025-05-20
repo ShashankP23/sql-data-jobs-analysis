0ed4956e6d46daa3e22dfdd2ce04dde9e40a908c
Shashank Priyadarshi <shashank.priyadarshi1098@gmail.com>
2025-05-20 22:53:29 +0530

    Update query 4

--- sql_queries/4_top_paying_skills.sql
+++ sql_queries/4_top_paying_skills.sql
@@ -3,11 +3,13 @@
 
 SELECT
     skills_dim.skills,
-    AVG(jobs.salary_year_avg) AS avg_salary
+    ROUND(AVG(jobs.salary_year_avg), 0) AS avg_salary
 FROM job_postings_fact AS jobs
 INNER JOIN skills_job_dim ON jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE jobs.job_title_short = 'Data Analyst'
+WHERE
+    jobs.job_title_short = 'Data Analyst'
+    AND jobs.salary_year_avg IS NOT NULL
 GROUP BY skills_dim.skills
 ORDER BY avg_salary DESC
-LIMIT 10;
+LIMIT 25;
